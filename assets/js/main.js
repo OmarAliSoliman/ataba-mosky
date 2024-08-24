baec005d8d214a81329345b9c7166e23fda8f7c9
OmarAliSoliman <omarasmar9955@gmail.com>
2024-08-24 21:51:42 +0300

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,8 +77,63 @@ $(document).ready(function () {
       autoplaySpeed: 5000,
       centerMode: false,
       autoplay: true,
-      // nextArrow: $(".custom_category_arrow .next"),
-      // prevArrow: $(".custom_category_arrow .prev"),
+      nextArrow: $(".custom_category_arrow1 .next"),
+      prevArrow: $(".custom_category_arrow1 .prev"),
+      responsive: [
+        {
+          breakpoint: 1920,
+          settings: {
+            slidesToShow: 5,
+            // centerMode: false,
+          },
+        },
+        {
+          breakpoint: 1150,
+          settings: {
+            slidesToShow: 4,
+            // centerMode: false,
+          },
+        },
+        {
+          breakpoint: 1100,
+          settings: {
+            slidesToShow: 3,
+            // centerMode: false,
+          },
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            // centerMode: false,
+          },
+        },
+
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            // centerMode: false,
+          },
+        },
+      ],
+    });
+  }
+  if($(".products_section_slider2").length){
+    $(".products_section_slider2").slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+      speed: 200,
+      infinite: true,
+      css: "linear",
+      loop: true,
+      autoplaySpeed: 5000,
+      centerMode: false,
+      autoplay: true,
+      nextArrow: $(".custom_category_arrow2 .next"),
+      prevArrow: $(".custom_category_arrow2 .prev"),
       responsive: [
         {
           breakpoint: 1920,
@@ -174,6 +229,18 @@ $(document).ready(function () {
         },
       ],
     });
+  }
+
+  if($(".product_details_pictures").length){
+    $(".big_slider").slick({
+      asNavFor: ".small_slider",
+    })
+    $(".small_slider").slick({
+      asNavFor: '.big_slider',
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      focusOnSelect: true,
+    })
   }
   
   // if($(".accordion_price").length){
